ea9136c166e1375612e21762b6dc3799c36320d1
JoaoVitorScuira <jvscuira@gmail.com>
2021-08-02 21:01:19 -0300

    Alteração via Interface

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace C_Sharp_Aulas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Versionamento de códigos!!");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # C-Sharp-Aulas
 Aulas de C# 
+#Exemplo de utilização do GitHub (Interface do VS Code)
